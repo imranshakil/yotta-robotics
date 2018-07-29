508b8228f90c09bda01608d369545fd57d3acb02
imranshakil <imranshakil77@gmail.com>
2018-07-29 14:31:58 +0600

    Change Templates

--- publicsite/urls.py
+++ publicsite/urls.py
@@ -5,4 +5,13 @@ from . import views
 
 urlpatterns = [
     url('', views.home, name='home'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^services/', views.services, name='services'),
+    url(r'^products/', views.products, name='products'),
+    url(r'^blog/', views.blog, name='blog'),
+    url(r'^contact/', views.contact, name='contact'),
+
+
+
+
 ]
--- publicsite/views.py
+++ publicsite/views.py
@@ -7,3 +7,22 @@ def home(request):
     title = "Welcome to Yotta Robotics"
     page = {"title":title}
     return render(request, 'publicsite/index.html', {'name': home,'page':page})
+
+def about(request):
+    return render(request, 'publicsite/commom/about.html', {'about': about})
+
+def services(request):
+    return render(request, 'publicsite/commom/services.html', {'services': services})
+
+def products(request):
+    return render(request, 'publicsite/products.html', {'products': products})
+
+def blog(request):
+    return render(request, 'blog/blog-home.html', {'blog': blog})
+
+def contact(request):
+    return render(request, 'publicsite/commom/contact.html', {'contact': contact})
+
+
+def customeradd(request):
+    return render(request, 'modules/customers/customer_add.html')
